2d72530144c8bcb797cd0f9fbfa83d687ac0a93b
Olegsandrik <Nikitinnvlalex@gmail.com>
2023-11-06 21:10:04 +0300

    Продолжение выполнения ДЗ номер2

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index),
-    path('about-us', views.about)
+    path('', views.base, name='home'),
+    path('ask/', views.ask, name='ask'),
+    path('login/', views.login, name='login'),
+    path('singup/', views.singup, name='singup'),
+    path('hot/', views.hot, name='hot'),
+    path('tag/<str:tagname>/', views.tag, name='tag'),
+    path('question/<int:number>/', views.question, name='question'),
 ]
