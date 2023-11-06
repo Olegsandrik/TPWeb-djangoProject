7fe60c1cb3954d90ef2267d90b9d7085bbd55801
Olegsandrik <Nikitinnvlalex@gmail.com>
2023-11-06 13:53:34 +0300

    Начало выполнения ДЗ номер2

--- djangoProjectTPWeb/settings.py
+++ djangoProjectTPWeb/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret! Вы этого не видели
 SECRET_KEY = 'django-insecure-z$r9qj)!$*(rzy0vd_j8mcgmanx9et-e^rh%k7ose+^xnb^ivl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app'
 ]
+
+# подключил приложение app
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -104,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
